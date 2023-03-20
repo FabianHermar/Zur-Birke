f48259c462ff74e75faaeebe09b092e77e6f14e9
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2023-03-20 00:52:25 -0600

    🆕 components connected

---
+++ app/page.jsx
@@ -0,0 +1,15 @@
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
+import Info from '@/components/Info'
+import Principal from '@/components/Principal'
+
+export default function Home() {
+  return (
+    <main>
+      <Header />
+      <Principal />
+      <Info />
+      <Footer />
+    </main>
+  )
+}
