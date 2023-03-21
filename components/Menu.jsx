fa9e4e9ab2b1975cb8cfdc76dc84d4d2351a03b9
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2023-03-21 01:16:34 -0600

    🆕 Menu component added

---
+++ components/Menu.jsx
@@ -0,0 +1,10 @@
+export default function Menu() {
+  return (
+    <div className='menu'>
+      <h2 className='text-5xl font-[PlayFair] text-center mb-5'>Our Menu</h2>
+      <div className='grid grid-col-1 lg:grid-cols-4 gap-4 justify-items-center'>
+        <article></article>
+      </div>
+    </div>
+  )
+}
