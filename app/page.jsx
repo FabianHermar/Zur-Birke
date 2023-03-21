1876aee87b783f37891ec292fe99001fc3d115d1
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2023-03-21 01:14:47 -0600

    🔌 Menu component connected

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,7 @@
 import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import Info from '@/components/Info'
+import Menu from '@/components/Menu'
 import Principal from '@/components/Principal'
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <Header />
       <Principal />
       <Info />
+      <Menu />
       <Footer />
     </main>
   )
