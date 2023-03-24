827a73fecb2e4fdd117e3b9b1aa29452da4307a3
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2023-03-23 23:48:32 -0600

    ✅ menu component finished

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -1,10 +1,39 @@
+import Image from 'next/image'
+
 export default function Menu() {
   return (
-    <div className='menu'>
-      <h2 className='text-5xl font-[PlayFair] text-center mb-5'>Our Menu</h2>
-      <div className='grid grid-col-1 lg:grid-cols-4 gap-4 justify-items-center'>
-        <article></article>
+    <section className='menu' id='menu'>
+      <h2 className='text-5xl font-[PlayFair] text-center mb-10'>Our Menu</h2>
+      <div className='select-none'>
+        <picture className='flex flex-col lg:flex-row lg:justify-around'>
+          <Image
+            className='drop-shadow-xl shadow-black border-8 border-double border-[#deb887]/80'
+            src='/Zur-Birke-Menu.webp'
+            alt='Zur Birke Menu'
+            width={500}
+            height={500}
+          />
+          <Image
+            className='drop-shadow-xl shadow-black border-8 border-double border-[#deb887]/80'
+            src='/Zur-Birke-Menus.webp'
+            alt='Zur Birke Menu'
+            width={500}
+            height={500}
+          />
+        </picture>
+        <h3 className='text-5xl font-[PlayFair] text-center mb-10 mt-10'>
+          Lunch Menu
+        </h3>
+        <picture className='flex flex-col lg:flex-row lg:justify-around'>
+          <Image
+            className='drop-shadow-xl shadow-black border-8 border-double border-[#deb887]/80'
+            src='/LunchMenu.webp'
+            alt='Lunch Menu'
+            width={500}
+            height={500}
+          />
+        </picture>
       </div>
-    </div>
+    </section>
   )
 }
