6edadadf228afc7bc3502297b1f8b33f9cbd03d3
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2023-03-26 00:30:16 -0600

    🔌 payments and schedule components connected

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,9 @@ import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import Info from '@/components/Info'
 import Menu from '@/components/Menu'
+import Payments from '@/components/Payments'
 import Principal from '@/components/Principal'
+import Schedules from '@/components/Schedules'
 
 export default function Home() {
   return (
@@ -11,6 +13,8 @@ export default function Home() {
       <Principal />
       <Info />
       <Menu />
+      <Schedules />
+      <Payments />
       <Footer />
     </main>
   )
