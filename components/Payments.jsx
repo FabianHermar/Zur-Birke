10d7a1f80164f558ce7e2f857d17674b3b5f55f9
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2023-05-02 21:50:27 -0600

    🛠️ images routes repared

--- components/Payments.jsx
+++ components/Payments.jsx
@@ -6,14 +6,14 @@ export default function Payments() {
       <h2 className='text-5xl font-[PlayFair] text-center mb-10'>
         Available payment methods
       </h2>
-      <ul>
-        <li className='flex items-center justify-center mb-10'>
-          <Image
-            src='/cash-icon.svg'
-            alt='Cash-icon'
-            width={100}
-            height={100}
-          />
+      <ul className='flex items-center justify-center mb-10 select-none opacity-80'>
+        <li className='flex justify-center items-center flex-col'>
+          <Image src='/cash-icon.svg' alt='Cash' width={70} height={70} />
+          <p className='font-[SegoeBold] text-gray-800 text-xl'>Cash</p>
+        </li>
+        <li className='ml-16 flex justify-center items-center flex-col'>
+          <Image src='/mc_ec.png' alt='EC card' width={70} height={70} />
+          <p className='font-[SegoeBold] text-gray-800 text-xl'>EC card</p>
         </li>
       </ul>
     </section>
